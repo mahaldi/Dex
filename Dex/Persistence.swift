62f076d2f090ea75def5c90cf5c47b859b679295
Mahaldi WIjaya Saputra <aldiwijayasaputra@gmail.com>
2026-01-27 23:33:48 +0700

    Finish core data app and widget

--- Dex/Persistence.swift
+++ Dex/Persistence.swift
@@ -54,6 +54,8 @@ struct PersistenceController {
         container = NSPersistentContainer(name: "Dex")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
+        } else {
+            container.persistentStoreDescriptions.first!.url = FileManager.default.containerURL(forSecurityApplicationGroupIdentifier: "group.com.mahaldiapps.DexGroup")!.appending(path: "Dex.sqlite")
         }
         container.loadPersistentStores(completionHandler: { (storeDescription, error) in
             if let error = error as NSError? {
